1e39773773230c8049f3b7c9b61078af944d1c12
Lushen Pillay <lushenpillay09@gmail.com>
2024-03-14 18:02:26 +0200

    Update CORS policy

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000' })); 
+app.use(cors({ origin: 'https://crypto-watch-sable.vercel.app' })); 
 
 // Routes
 
